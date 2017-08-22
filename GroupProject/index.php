c43d4d0316e5b82e471eb26b26fe58e850010d20
yummy <yummy@10.17.2.201>
2017-08-21 20:43:35 -0500

    Beginning of Controller

--- GroupProject/index.php
+++ GroupProject/index.php
@@ -1,4 +1,16 @@
 <?php
 session_start();
 
+//gets initial page action to direct to main login
+$action = filter_input(INPUT_POST, 'action');
+
+//logic to navigate to initial login
+if($action === NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if($action === NULL)  {
+        $action = 'initial_login';
+    }
+}
+
+
 ?>
