7b407287a07712886b785c665d5ecf66e276d8cf
mm147988 <mm147988@LR00103.southeast.edu>
2017-08-21 21:14:55 -0500

    Started model classes

---
+++ GroupProject/Model/Waitlist.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Waitlist
+ *
+ * @author mm147988
+ */
+class Waitlist {
+    //put your code here
+}
