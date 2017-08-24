1508e88ed5a047a06c80f0c331f9bf2623fad25f
br590523 <br590523@LR00112.southeast.edu>
2017-08-23 20:54:55 -0500

    required store number to stop passing by it no validation on it though

--- GroupProject/Views/login.php
+++ GroupProject/Views/login.php
@@ -4,7 +4,7 @@
 <section id='login'>
     <form action="index.php" method="post">
         <label>Store Number: </label>
-        <input type="text" name="store_number">
+        <input type="text" name="store_number" required>
         
         <input type="hidden" name="action" value="Login">
         <input type="submit" value="Login">
