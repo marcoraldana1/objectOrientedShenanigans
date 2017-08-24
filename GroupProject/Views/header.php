8c50da63d68ffad71f2c0188608ca7e4ca6449f0
br590523 <br590523@LR00112.southeast.edu>
2017-08-23 20:28:15 -0500

    minor changes to header

--- GroupProject/Views/header.php
+++ GroupProject/Views/header.php
@@ -1,6 +1,8 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>O'Shea Shenanigans Pub & Grill</title>
+        <meta charset="utf-8">
         <link rel="stylesheet" href="Views/main.css">
     </head>
     <header>
