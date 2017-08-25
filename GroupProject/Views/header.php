b4e70102e5d4ab78d66f3f792150fe26bf11e21e
leona <leona@10.17.2.31>
2017-08-24 20:45:38 -0500

    Merge origin/master
    Conflicts:
    	GroupProject/Views/index.php

--- GroupProject/Views/header.php
+++ GroupProject/Views/header.php
@@ -3,10 +3,12 @@
     <head>
         <title>O'Shea Shenanigans Pub & Grill</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="Views/main.css">
+        <link rel="stylesheet" href="../style/main.css">
     </head>
     <header>
-        <h1>O'Shea Shenanigans Pub & Grill</h1>
+        <p>
+            <img  src="images/main_title1.png" alt="" width="1000px" height="100px">
+        </p>
     </header>
 
 
