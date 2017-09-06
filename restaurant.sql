bca4a0737b16146854c09e7b80aeb3114d965f77
leonardjj17 <jl64990@southeast.edu>
2017-09-06 02:13:56 -0500

    update data in store table

--- restaurant.sql
+++ restaurant.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 29, 2017 at 02:12 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: Sep 06, 2017 at 09:11 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `restaurant`
 --
-CREATE DATABASE IF NOT EXISTS `restaurant` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `restaurant`;
 
 -- --------------------------------------------------------
 
@@ -28,17 +26,15 @@ USE `restaurant`;
 -- Table structure for table `reservations`
 --
 
-CREATE TABLE IF NOT EXISTS `reservations` (
-  `resID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `reservations` (
+  `resID` int(11) NOT NULL,
   `custName` varchar(50) NOT NULL,
   `custPhone` varchar(10) DEFAULT NULL,
   `partySize` int(11) NOT NULL,
   `resDate` varchar(10) NOT NULL,
   `resTime` varchar(10) NOT NULL,
-  `storeNum` varchar(25) NOT NULL,
-  PRIMARY KEY (`resID`),
-  KEY `restostoreFK` (`storeNum`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `storeNum` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `reservations`
@@ -55,13 +51,11 @@ INSERT INTO `reservations` (`resID`, `custName`, `custPhone`, `partySize`, `resD
 -- Table structure for table `servers`
 --
 
-CREATE TABLE IF NOT EXISTS `servers` (
+CREATE TABLE `servers` (
   `serverID` varchar(15) NOT NULL,
   `storeNum` varchar(25) NOT NULL,
   `serverFName` varchar(50) NOT NULL,
-  `serverLName` varchar(50) NOT NULL,
-  PRIMARY KEY (`serverID`),
-  KEY `servertostoreFK` (`storeNum`)
+  `serverLName` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -70,8 +64,8 @@ CREATE TABLE IF NOT EXISTS `servers` (
 
 INSERT INTO `servers` (`serverID`, `storeNum`, `serverFName`, `serverLName`) VALUES
 ('5284', '100', 'Molly', 'McGreggor'),
-('7361', '100', 'Kieran', 'O\'Connel'),
-('8349', '100', 'Braiden', 'O\'Conner'),
+('7361', '100', 'Kieran', 'O''Connel'),
+('8349', '100', 'Braiden', 'O''Conner'),
 ('9358', '100', 'Caitlyn', 'McBeth');
 
 -- --------------------------------------------------------
@@ -80,13 +74,12 @@ INSERT INTO `servers` (`serverID`, `storeNum`, `serverFName`, `serverLName`) VAL
 -- Table structure for table `store`
 --
 
-CREATE TABLE IF NOT EXISTS `store` (
+CREATE TABLE `store` (
   `storeID` varchar(25) NOT NULL,
   `num2pTables` int(11) DEFAULT NULL,
   `num4pTables` int(11) DEFAULT NULL,
   `num6pTables` int(11) DEFAULT NULL,
-  `num8pTables` int(11) DEFAULT NULL,
-  PRIMARY KEY (`storeID`)
+  `num8pTables` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -94,10 +87,10 @@ CREATE TABLE IF NOT EXISTS `store` (
 --
 
 INSERT INTO `store` (`storeID`, `num2pTables`, `num4pTables`, `num6pTables`, `num8pTables`) VALUES
-('100', 4, 4, 2, 1),
+('100', 4, 10, 6, 2),
 ('101', 6, 4, 2, 0),
-('102', 4, 6, 4, 2),
-('103', 6, 4, 1, 1);
+('102', 7, 9, 5, 1),
+('103', 7, 12, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -105,16 +98,14 @@ INSERT INTO `store` (`storeID`, `num2pTables`, `num4pTables`, `num6pTables`, `nu
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL,
   `userRole` varchar(50) NOT NULL,
   `userName` varchar(50) NOT NULL,
   `userLogin` varchar(50) NOT NULL,
   `userPassword` varchar(50) NOT NULL,
-  `storeNum` varchar(25) NOT NULL,
-  PRIMARY KEY (`userID`),
-  KEY `usertostoreFK` (`storeNum`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `storeNum` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -123,8 +114,56 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`userID`, `userRole`, `userName`, `userLogin`, `userPassword`, `storeNum`) VALUES
 (1, 'Host', 'Sara', 'Sara123', 'imawesome', '100'),
 (2, 'Manager', 'Mr. Big', 'BigBusiness', 'BigMoney', '100'),
-(3, 'Server', 'Server Station 1', 'serverstation1', 'letmein', '100');
+(3, 'Server', 'Server Station 1', 'serverstation1', 'letmein', '100'),
+(4, 'Manager', 'Tom O''Henry', 'BigO', 'guiness1', '101'),
+(5, 'Manager', 'Robbie', 'RMurphy', 'rugbyRules', '102'),
+(6, 'Manager', 'Kevin', 'Killian1', 'red99', '103');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`resID`),
+  ADD KEY `restostoreFK` (`storeNum`);
+
+--
+-- Indexes for table `servers`
+--
+ALTER TABLE `servers`
+  ADD PRIMARY KEY (`serverID`),
+  ADD KEY `servertostoreFK` (`storeNum`);
+
+--
+-- Indexes for table `store`
+--
+ALTER TABLE `store`
+  ADD PRIMARY KEY (`storeID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`),
+  ADD KEY `usertostoreFK` (`storeNum`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `resID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- Constraints for dumped tables
 --
