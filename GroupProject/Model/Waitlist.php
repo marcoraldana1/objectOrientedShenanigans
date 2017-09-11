24f7be2971f5b750847b4ed189498aefd6f654cd
Michael <Michael@Michael-PC>
2017-09-10 23:04:45 -0500

    fixed deleting from waitlist, moved some buttons around on waitlist

--- GroupProject/Model/Waitlist.php
+++ GroupProject/Model/Waitlist.php
@@ -1,17 +1,13 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Waitlist
  *
  * @author mm147988
  */
 class Waitlist {
+    //rather than use two arrays, it would be better to
+    //use a linked list, but I doubt I would have time to
+    //implement that before this is due..
     private $reservedWaits;
     private $unreservedWaits;
     
@@ -56,27 +52,64 @@ class Waitlist {
     
     public function removeWaitByIndex($index) {
         $reserves = count($this->reservedWaits);
+        $i = 0;
+        $new = array();
+        //if the customer doesn't have a reservation..
         if ($index >= $reserves) {
             $index -= $reserves;
-            return $this->unreservedWaits[$index];
+            
+            if ($index >= count($this->unreservedWaits)) {
+                //this shouldn't happen if index is valid
+                return;
+            }
+            
+            //go through all the waits and copy all except $index (reindexes)
+            foreach($this->unreservedWaits as $wait) {
+                if ($i != $index) {
+                    $new[$i] = $wait;
+                }
+                $i++;
+            }
+            $this->unreservedWaits = $new;
         }
+        //if they do have a reservation
         else {
-            return $this->reservedWaits[$index];
+            foreach($this->reservedWaits as $wait) {
+                if ($i != $index) {
+                    $new[$i] = $wait;
+                }
+                $i++;
+            }
+            $this->reservedWaits = $new;
         }
     }
     
     //remove a customer from the waitlist and return true, return false if failed
     public function removeFromWaitlist($customer) {
+        $new = array();
+        $i = 0;
         //if they have a reservation, and are in the wait list
         if ($index = array_search($customer, $this->reservedWaits) !== false) {
             //remove them
-            unset($this->reservedWaits[$index]);
+            foreach($this->reservedWaits as $wait) {
+                if ($wait != $customer) {
+                    $new[$i] = $wait;
+                }
+                $i++;
+            }
+            $this->reservedWaits = $new;
             //and return 1
             return true;
         }
         //no reservation, but in the waitlist
         elseif ($index = array_search($customer, $this->unreservedWaits) !== false) {
-            unset($this->unreservedWaits[$index]);
+            foreach($this->unreservedWaits as $wait) {
+                if ($wait != $customer) {
+                    $new[$i] = $wait;
+                }
+                $i++;
+            }
+            $this->unreservedWaits = $new;
             return true;
         }
         //not in the waitlist
--- GroupProject/Views/Waitlist.php
+++ GroupProject/Views/Waitlist.php
@@ -1,4 +1,10 @@
-<h2>Waitlist</h2>
+
+    <form action="index.php"  method="post">
+        <h2>Waitlist
+        <input type='hidden' name='action' value='addWait'>
+        <input type='submit' class='addWait' value='ADD'> 
+        </h2>
+    </form>
 <hr>
 <div class="wait">
     <?php
@@ -6,11 +12,11 @@
     foreach ($currentWaitlist as $index=>$customer):
         ?>
         <p>
+            <form action='index.php' method='post'>
             <?php echo $customer->getCustomerName() ?>
             <br>Party of 
             <?php echo $customer->getPartySize() ?>
             <br>
-        <form action='index.php' method='post'>
             <input type='hidden' name='action' value='deleteWait'>
             <input type='hidden' name='index' value='<?php echo $index; ?>'>
             <input type='submit' class='deleteWait' value='DELETE'>
@@ -18,8 +24,5 @@
         </p>
 
     <?php endforeach; ?>
-    <form action="index.php"  method="post">
-        <input type='hidden' name='action' value='addWait'>
-        <input type='submit' class='addWait' value='ADD'> 
-    </form>
+
 </div>
